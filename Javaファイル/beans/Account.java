5f239f6a4ce61287a10eb1205c5be93e9bf43ed4
azuresamples <54381444+azuresamples@users.noreply.github.com>
2019-11-01 17:21:41 +0900

    Add files via upload

--- Javaファイル/beans/Account.java
+++ Javaファイル/beans/Account.java
@@ -1,1 +1,37 @@
+package com.ems.beans;
 
+import java.io.Serializable;
+
+public class Account implements Serializable{
+
+	private int account_id;
+	private String name;
+	private String password;
+	private int authority;
+	public int getAccount_id() {
+		return account_id;
+	}
+	public void setAccount_id(int account_id) {
+		this.account_id = account_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getAuthority() {
+		return authority;
+	}
+	public void setAuthority(int authority) {
+		this.authority = authority;
+	}
+
+
+}
---
+++ Javaファイル/beans/AutoComplete.java
@@ -0,0 +1,47 @@
+package com.ems.beans;
+
+public class AutoComplete {
+	private String serial_no;
+	private String category_name;
+	private String manufacturer;
+	private String name;
+	private String spec;
+	private String purchase_date;
+
+	public String getSerial_no() {
+		return serial_no;
+	}
+	public void setSerial_no(String serial_no) {
+		this.serial_no = serial_no;
+	}
+	public String getCategory_name() {
+		return category_name;
+	}
+	public void setCategory_name(String category_name) {
+		this.category_name = category_name;
+	}
+	public String getManufacturer() {
+		return manufacturer;
+	}
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSpec() {
+		return spec;
+	}
+	public void setSpec(String spec) {
+		this.spec = spec;
+	}
+	public String getPurchase_date() {
+		return purchase_date;
+	}
+	public void setPurchase_date(String purchase_date) {
+		this.purchase_date = purchase_date;
+	}
+}
